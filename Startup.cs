a4d3fff86af352998143ed5d3ce0eaf2a9b58e62
Adrian D. Alvarez <adrian@adriandalvarez.com>
2019-09-08 19:15:30 -0400

    Redirect /resume to pdf resume

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Net.Http.Headers;
 
 namespace AdrianDAlvarez
 {
@@ -28,7 +30,7 @@ namespace AdrianDAlvarez
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
+                options.MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.None;
             });
 
             services.AddResponseCompression();
@@ -49,6 +51,22 @@ namespace AdrianDAlvarez
                 // TODO: The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            var options = new RewriteOptions()
+               .Add(context =>
+               {
+                    var request = context.HttpContext.Request;
+
+                    if (request.Path.Value.Contains("/resume", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var response = context.HttpContext.Response;
+                        response.StatusCode = StatusCodes.Status307TemporaryRedirect;
+                        context.Result = RuleResult.EndResponse;
+                        response.Headers[HeaderNames.Location] = "/Content/pdfs/AdrianDAlvarez_Resume.pdf";
+                    }
+               });
+
+            app.UseRewriter(options);
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
